3db09fae14d8d07f89750d42579ce0c520da342a
Singh Manas Manoj <singhmanas2786@gmail.com>
2026-07-25 21:05:00 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,32 @@
+class TimeMap {
+public:
+unordered_map<string, vector<pair<int, string>>> mp;
+// priority_queue<pair<int, string>> pq; // max heap having {timestamps, key}
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        auto timeValVec = mp[key];
+        int low = 0;
+        int high = timeValVec.size() - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (timeValVec[mid].first <= timestamp) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans == -1 ? "" : timeValVec[ans].second;
+    }
+};
